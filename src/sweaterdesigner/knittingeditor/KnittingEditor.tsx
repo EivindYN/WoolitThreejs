59be0bcbc89cb0981d177bca85f7c1e9b6d71b80
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2023-03-02 14:02:06 +0100

    Add first brush ui

--- src/sweaterdesigner/knittingeditor/KnittingEditor.tsx
+++ src/sweaterdesigner/knittingeditor/KnittingEditor.tsx
@@ -6,7 +6,13 @@ import { loadGrid } from './gridcanvas';
 
 function KnittingEditor(props: any) {
 
-    const [grid, setGrid] = useState(new Array(90).fill(0).map(() => new Array(90).fill(0)));
+    function make2DArray(x: number, y: number) {
+        return new Array(y).fill(0).map(() => new Array(x).fill(0))
+    }
+
+    const [grid, setGrid] = useState(make2DArray(90, 90));
+    const [brush, setBrush] = useState(undefined)
+    const [showBrushPopup, setShowBrushPopup] = useState(false)
 
     useEffect(() => {
         if (!props.selectedPattern) return
@@ -17,8 +23,30 @@ function KnittingEditor(props: any) {
 
     return (
         <div style={{ height: "100vh", minWidth: "50%", backgroundColor: "#f9f5f2", maxWidth: "50%" }}>
+            {showBrushPopup &&
+                <div
+                    className='box'
+                    style={{
+                        position: "absolute",
+                        marginLeft: "10px",
+                        marginTop: "70px",
+                        width: "300px",
+                        height: "80px",
+                        backgroundColor: "white"
+                    }}>
+                    <div style={{ display: "flex" }}>
+                        <button onClick={() => setShowBrushPopup(false)}>
+                            <img src="paint.png" style={{
+                                width: "30px",
+                                imageRendering: "crisp-edges"
+                            }}></img>
+                        </button>
+                        {/*<hr></hr>*/}
+                    </div>
+                </div>
+            }
             <div style={{ display: "flex" }}>
-                <button style={{ marginRight: "0px" }} onClick={() => console.log("hi")}>
+                <button style={{ marginRight: "0px" }} onClick={() => setShowBrushPopup(true)}>
                     <img src="brush.png" style={{ width: "30px" }}></img>
                 </button>
                 <button style={{ marginRight: "0px", borderTopRightRadius: "0px", borderBottomRightRadius: "0px" }}>
